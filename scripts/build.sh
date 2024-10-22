52a3536788399a1d5c73c0864e353539e71e6a9d
richerfu <southorange0929@foxmail.com>
2024-10-22 15:23:49 +0800

    fix(example): fix example build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+script_dir=$(dirname "$0")
+
+script_dir=$(cd "$script_dir" && pwd)
+
+parent_dir=$(dirname "$script_dir")
+
+cd $parent_dir/example
+
 rm -rf build
 
 mkdir build && pushd build
 
-cmake -DOHOS_STL=c++_shared -DOHOS_ARCH=arm64-v8a  ../
+cmake -DOHOS_STL=c++_shared -DOHOS_ARCH=arm64-v8a ../
 
 cmake --build ./
